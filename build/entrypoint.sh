fe32b89881325f2736baab323e472bef60587a9e
iamEvan <chanevan0819@gmail.com>
2024-05-28 18:22:40 +0100

    fix session

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-# Installs Bitwarden CLI
-npm install -g @bitwarden/cli
-
 # Function to handle termination signals
 terminate() {
   echo "Terminating the Bitwarden service..."
@@ -18,9 +15,6 @@ bw config server ${BW_SERVER}
 
 # Login to Bitwarden
 bw login --apikey ${BW_CLIENTID} ${BW_CLIENTSECRET}
-
-# Unlock Bitwarden vault
-bw unlock --passwordenv ${BW_PASSWORD}
 
 # Start the Bitwarden service
 bw serve --port ${PORT} --hostname ${HOSTNAME} &
