76af7d6bd5465bec779f96274019015034b400d3
iamEvan <chanevan0819@gmail.com>
2024-05-28 17:43:36 +0100

    fix stopping container

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -1,4 +1,14 @@
- #!/bin/sh
+#!/bin/sh
+
+# Function to handle termination signals
+terminate() {
+  echo "Terminating the Bitwarden service..."
+  # Add any cleanup tasks here if needed
+  exit 0
+}
+
+# Trap termination signals (SIGTERM, SIGINT)
+trap terminate TERM INT
 
 # Configure Bitwarden server
 bw config server ${BW_SERVER}
@@ -10,4 +20,7 @@ bw login --apikey ${BW_CLIENTID} ${BW_CLIENTSECRET}
 bw unlock --passwordenv ${BW_PASSWORD}
 
 # Start the Bitwarden service
-bw serve --port ${PORT} --hostname ${HOSTNAME}
+bw serve --port ${PORT} --hostname ${HOSTNAME} &
+
+# Wait for the background process to finish
+wait $!
